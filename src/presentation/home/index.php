ee7f16a61785dc89509ae272cdce0294fea9c667
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-08 23:21:49 -0500

    Eliminación del archivo .gitkeep y actualización del contenido en index.php, cambiando el texto de "Hello World" a "Texto de test" y mejorando el diseño con nuevas clases y animaciones en los botones.

--- src/presentation/home/index.php
+++ src/presentation/home/index.php
@@ -11,12 +11,11 @@
 
 <body x-data="{ show: false }">
     <div class="h-screen w-screen bg-gray-100 flex justify-center items-center">
-        <div class="bg-gray-50 text-black mx-auto rounded-lg p-4">
-            <h2 class="text-2xl font-bold">Hello World</h2>
-            <button @click="show = !show" class="bg-blue-500 text-white px-4 py-2 rounded-md">Show</button>
-            <div x-show="show">
+        <div class="bg-gray-50 text-black mx-auto rounded-lg p-4 w-1/2 flex justify-center flex-col items-center gap-5">
+            <h2 class="text-2xl font-bold">Texto de test</h2>
+            <button @click="show = !show" class="bg-blue-500 text-white px-4 py-2 rounded-md transition-all duration-300">Mostrar</button>
+            <div x-show="show" class="transition-all duration-300 animate-bounce">
                 <h1>Hello World</h1>
-                <button @click="show = !show" class="bg-red-500 text-white px-4 py-2 rounded-md">Hide</button>
             </div>
         </div>
     </div>
