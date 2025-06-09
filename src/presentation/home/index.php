aed1aef8cc67d676d4bbac6073b417fd466d74fa
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-08 23:11:55 -0500

    Actualización de dependencias de Tailwind CSS y ajustes en la estructura de archivos CSS y PHP para mejorar la presentación de la página de inicio.

--- src/presentation/home/index.php
+++ src/presentation/home/index.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body>
-    <div>
+    <div class="h-screen w-screen bg-gray-100 flex justify-center items-center">
+       <div class="bg-red-500 text-white mx-auto rounded-lg p-4">
         <h2 class="text-2xl font-bold">Hello World</h2>
-    </div>
+       </div>
+    </div>  
 </body>
 </html>
