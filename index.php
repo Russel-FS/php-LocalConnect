de2d24de9ec5f77febaf547e241911078804221d
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-16 17:39:15 -0500

    Actualización de comentarios en index.php para mayor claridad en la gestión de sesiones y carga de rutas.

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
-
-// Iniciar sesión
+// Iniciar la sesión para manejar la autenticación
 session_start();
-
 // Definir la constante de la ruta base
 define('BASE_PATH', __DIR__);
-
-// Cargar el archivo de rutas
+// Cargar el router y las rutas
 require_once __DIR__ . '/src/application/routes/routes.php';
