bac94fa052d5917900cfaa8dbac93e4b256e7abf
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-18 14:40:50 -0500

    feat: Update HomeController to return the correct view and restructure home page layout

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,31 +34,5 @@ class User extends Authenticatable
         'estado' => 'string'
     ];
 
-    // Relación con negocios (si el usuario es de tipo 'negocio')
-    public function negocio()
-    {
-        return $this->hasOne(Negocio::class, 'id_usuario');
-    }
 
-    // Relación con valoraciones hechas por el usuario
-    public function valoraciones()
-    {
-        return $this->hasMany(Valoracion::class, 'id_usuario');
-    }
-
-    // Método helper para verificar el tipo de usuario
-    public function isNegocio()
-    {
-        return $this->tipo === 'negocio';
-    }
-
-    public function isAdmin()
-    {
-        return $this->tipo === 'admin';
-    }
-
-    public function isResidente()
-    {
-        return $this->tipo === 'residente';
-    }
 }
