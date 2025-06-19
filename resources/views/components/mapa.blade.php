7d933ebe6e20e1bb0596f58cff69afc209feb593
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-19 15:55:08 -0500

    refactor: Añadir funcionalidad para obtener dirección a partir de coordenadas en mapa.blade.php
    - Se implementó la función obtenerDireccionDesdeCoordenadas para obtener y mostrar la dirección, distrito, ciudad, provincia, departamento y país a partir de las coordenadas del mapa.
    - Se mejoró la inicialización del mapa al esperar a que el DOM esté listo antes de cargarlo.

--- resources/views/components/mapa.blade.php
+++ resources/views/components/mapa.blade.php
@@ -3,6 +3,7 @@
     let map = null;
     let marker = null;
 
+    // Inicializar el mapa y el marcador
     function inicializarMapa() {
         const latitudDefault = -12.0464;
         const longitudDefault = -77.0428;
@@ -44,6 +45,7 @@
             map.invalidateSize();
         }, 100);
     }
+    // Esperar a que el que el domn este listo para inicializar el mapa
     document.addEventListener("DOMContentLoaded", function() {
         if (document.getElementById('map')) {
             inicializarMapa();
@@ -62,5 +64,41 @@
                 document.getElementById('longitud').value = lng.toFixed(6);
             }
         }
+        obtenerDireccionDesdeCoordenadas(lat, lng);
+    }
+
+    // funcion para obtener direccion de las coordenadas
+    function obtenerDireccionDesdeCoordenadas(lat, lng) {
+        const mapboxKey = 'pk.eyJ1IjoicnVzc2VsLWZzIiwiYSI6ImNtYTJ5djZ3NDFidzcybHNmZjl6dTEweGkifQ.L1_wuZGVMGSOmSKazwjxJg';
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${mapboxKey}&language=es&country=PE`)
+            .then(res => res.json())
+            .then(data => {
+                if (data.features && data.features.length > 0) {
+                    let direccion = '';
+                    let distrito = '';
+                    let ciudad = '';
+                    let provincia = '';
+                    let departamento = '';
+                    let pais = '';
+
+                    // filtrar los datos encontrados
+                    data.features.forEach(f => {
+                        if (f.place_type.includes('address')) direccion = f.place_name;
+                        if (f.place_type.includes('place')) ciudad = f.text;
+                        if (f.place_type.includes('region')) departamento = f.text;
+                        if (f.place_type.includes('district')) distrito = f.text;
+                        if (f.place_type.includes('locality')) provincia = f.text;
+                        if (f.place_type.includes('country')) pais = f.text;
+                    });
+
+                    // datos encontrados mostrar al input 
+                    if (document.getElementById('direccion')) document.getElementById('direccion').value = direccion;
+                    if (document.getElementById('distrito')) document.getElementById('distrito').value = distrito;
+                    if (document.getElementById('ciudad')) document.getElementById('ciudad').value = ciudad;
+                    if (document.getElementById('provincia')) document.getElementById('provincia').value = provincia;
+                    if (document.getElementById('departamento')) document.getElementById('departamento').value = departamento;
+                    if (document.getElementById('pais')) document.getElementById('pais').value = pais;
+                }
+            });
     }
 </script>
