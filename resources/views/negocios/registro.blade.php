c4fda89f356a75fb5a825f7037ef5b24a768cc35
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-20 12:44:32 -0500

    refactor: Añadir botón para mostrar resumen de negocio en registro.blade.php
    - Se incorporó un botón que permite al usuario mostrar el resumen del negocio, mejorando la interacción y revisión de datos antes de guardar.

--- resources/views/negocios/registro.blade.php
+++ resources/views/negocios/registro.blade.php
@@ -375,6 +375,7 @@
                         <p class="text-gray-600 mb-8">Revisa que todos los datos sean correctos</p>
 
                         <div id="resumen-negocio" class="space-y-6"></div>
+                        <button class="px-2 bg-green-900 rounded-full text-white" type="button" onclick="mostrarResumen()">mostrar resumen</button>
                         <div class="flex justify-between mt-10">
                             <button class="btn-premium-outline" type="button" onclick="cambiarPaso(6,5)">Anterior</button>
                             <button class="btn-premium" type="submit">Guardar negocio</button>
