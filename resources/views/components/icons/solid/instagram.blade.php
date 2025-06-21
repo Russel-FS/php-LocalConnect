4fb30908a2d1b21f525c3f1af93b6bf13e5734a4
Russel-Fs <russelfloressolano900@gmail.com>
2025-06-21 15:07:15 -0500

    refactor: Actualizar atributos de íconos SVG para mejorar la consistencia visual
    - Se eliminaron las clases de tamaño de los atributos de los íconos SVG de cog, globe, phone, facebook, instagram y whatsapp, reemplazándolas por un uso más directo de los atributos.
    - Se optimizó la estructura de los íconos para mantener una presentación más uniforme y simplificada en la interfaz.

--- resources/views/components/icons/solid/instagram.blade.php
+++ resources/views/components/icons/solid/instagram.blade.php
@@ -1,4 +1,4 @@
-<svg {{ $attributes->merge(['class' => 'w-6 h-6']) }} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+<svg {{ $attributes }} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
     <rect x="2" y="2" width="20" height="20" rx="6" />
     <circle cx="12" cy="12" r="5" />
     <circle cx="17.5" cy="6.5" r="1.5" fill="currentColor" />
