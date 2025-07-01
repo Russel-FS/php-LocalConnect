1c87a794058fbc13651a13386e593a046c65ceee
Russel-Fs <russelfloressolano900@gmail.com>
2025-07-01 02:07:43 -0500

    refactor: Eliminar migración de la tabla negocio_estadisticas
    - Se eliminó la migración correspondiente a la tabla negocio_estadisticas, ya que no es necesaria en la estructura actual de la base de datos.

---
+++ app/Models/Negocio/NegocioVista.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models\Negocio;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class NegocioVista extends Model
+{
+    protected $table = 'negocio_vistas';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'id_negocio',
+        'tipo_vista',
+        'id_usuario',
+    ];
+
+    public function negocio()
+    {
+        return $this->belongsTo(Negocio::class, 'id_negocio');
+    }
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'id_usuario');
+    }
+}
